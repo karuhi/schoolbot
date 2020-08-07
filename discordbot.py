18f2db4dd2782f7ecd8acbde7a8543296304be56
karuhi <h2401892@outlook.jp>
2020-08-07 23:38:30 +0900

    :bug: timezone関係を修正

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import tasks, commands
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import asyncio
 import os
 
@@ -27,7 +27,8 @@ async def on_ready():
 @tasks.loop(seconds=30.0)
 async def loop():
     # 現在の時刻
-    date = datetime.now(datetime.timedelta(hours=9))
+    JST = timezone(timedelta(hours=+9), 'JST')
+    date = datetime.now(JST)
     time = date.strftime('%H:%M')
     yobi = date.weekday()
     # 平日
